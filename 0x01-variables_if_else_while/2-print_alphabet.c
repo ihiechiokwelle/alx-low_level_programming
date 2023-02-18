48876dd2e7d4f0ad6ca72844104cc5a769fc6c8e
ihiechiokwelle <ihiechiokwelle@gmail.com>
2023-02-17 18:11:24 -0800

    Hades

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
- *
+ * Description: prints the alphabet in lowercase\n
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	char c;
+	int ch;
 
-	c = 'a';
-	while
-		(c <= '2') {
-			putchar(c);
-			c++;
-		}
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
