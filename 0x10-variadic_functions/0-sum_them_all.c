561d48bf34f4faaa056e5499adc9c086d0fe9345
ihiechiokwelle <ihiechiokwelle@gmail.com>
2023-03-23 04:33:12 -0700

    Good News

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,12 +17,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 
 	if (n == 0)
-		return (0);
+	return (0);
 
 	sum = 0;
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
+	sum += va_arg(args, int);
 
 	va_end(args);
 	return (sum);
